8e6057b15214a83ab7480f3058ad69edd8f36b6f
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-20 10:11:59 +0530

    updated routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,3 @@
-// const express = require("express");
-// const userRouter = require('./user');
-// const accountRouter = require ('./account');
-// // const app =express();
-
-// const router = express.Router();
-
-// router.use('/user' , userRouter);
-// router.use('/account', accountRouter)
-
-
-// module.exports = router;
-
-// backend/user/index.js
 const express = require('express');
 const userRouter = require("./user");
 const accountRouter = require("./account");
