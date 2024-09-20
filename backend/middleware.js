c516430d06f3ee6180ae68a1cbad17e1c2f13312
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-20 10:01:17 +0530

    authMiddleware for routes

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,34 +1,63 @@
-// authMiddelware
-const {JWT_SECRET} = require('./config');
+// // authMiddelware
+// const {JWT_SECRET} = require('./config');
+// const jwt = require("jsonwebtoken");
+
+
+// const authMiddelware = (req,res,next)=>{
+//     const authHeader = req.headers.authorization;
+
+//     if(!authHeader || !authHeader.startsWith('Bearer ')){
+//         return res.status(403).json({});
+//     }
+
+//     // to seprate info in headers.aut...
+//     // i.e auth type => like Bearer
+//     //          token => to be verified    
+//     const token = authHeader.split(' ')[1];
+    
+//     try {
+//         const decoded = jwt.verify(token, JWT_SECRET);
+//         if(decoded.userId){
+//             req.userId = decoded.userId;
+//             next();
+//         } else{
+//             return res.status(403).json({});
+//         }
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(403).json({});
+//     }
+// };
+
+// module.exports ={
+//     authMiddelware
+// }
+
+
+
+const { JWT_SECRET } = require("./config");
 const jwt = require("jsonwebtoken");
 
-
-const authMiddelware = (req,res,next)=>{
+const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(403).json({});
     }
 
-    // to seprate info in headers.aut...
-    // i.e auth type => like Bearer
-    //          token => to be verified    
     const token = authHeader.split(' ')[1];
-    
+
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
-        if(decoded.userId){
-            req.userId = decoded.userId;
-            next();
-        } else{
-            return res.status(403).json({});
-        }
-    } catch (error) {
-        console.log(error);
+
+        req.userId = decoded.userId;
+
+        next();
+    } catch (err) {
         return res.status(403).json({});
     }
 };
 
-module.exports ={
-    authMiddelware
+module.exports = {
+    authMiddleware
 }
