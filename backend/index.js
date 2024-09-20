38269f6dfc7fef7fded83587c0937155f820f4fb
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-20 09:59:44 +0530

    server endpoint updated

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const mainRouter = require("./routes/index");
-const cors = requrie ('cors');
-const app  = express();
+
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("api/v1" ,mainRouter);
-app.listen(3000);
+
+app.use("/api/v1", rootRouter);
+
+app.listen(3001);
