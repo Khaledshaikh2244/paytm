b86b0af0ecc29ed680cc726ad4de195a650bcb07
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-09-21 13:06:34 +0530

    added frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import {
+BrowserRouter,
+Route,
+Routes,
+} from "react-router-dom";
+
+import {Signup} from "../src/pages/Signup";
+import {Singin} from "../src/pages/Singin";
+import  {Dashboard} from "../src/pages/Dashboard";
+import { SendMoney } from "../src/pages/SendMoney";
+
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+    
+        <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Singin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+
+        </Routes>
+        </BrowserRouter>
+        
+    </>
   )
 }
 
